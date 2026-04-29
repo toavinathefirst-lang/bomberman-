a83a027bfdabc680f06d933cdbb73782c26da06d
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-04-30 01:31:33 +0300

    amelioration de l IA et du deplacement des ennemis

--- script/enemy.gd
+++ script/enemy.gd
@@ -3,9 +3,14 @@ extends KinematicBody2D
 
 
 var canMove:bool= true
-var dir:int = 0
+var dirXY:Vector2=Vector2()
+var list:Array=[]
+var oldDir = Vector2()
 onready var tw:Tween=  $tw
+onready var colladerDown:RayCast2D = $RayDown
 onready var colladerRight:RayCast2D = $RayRight
+onready var colladerLeft:RayCast2D = $RayLeft
+onready var colladerUp:RayCast2D = $RayUp
 onready var animator:AnimationPlayer = $animator
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,39 +21,32 @@ func _ready():
 	pass # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	movement()
 	
-	pass
-	var vel = Vector2()
-	if dir==0 && !$RayRight.is_colliding() :
-		vel.x+=1
-		
-	elif dir ==1 && !$RayLeft.is_colliding() : 
-		vel.x -=1 
-		
-	elif dir == 2 && !$RayDown.is_colliding() : 
-		vel.y +=  1 
-	
-	elif dir == 3 && !$RayUp.is_colliding(): 
-		vel.y -=  1 
-
-	movement(vel)
-	
-func movement(vec):
+func movement():
 	if canMove == true :
+		if (list.size()>0):list.clear()
+		if (!colladerRight.is_colliding()):
+			list.append(Vector2(1,0))
+		if (!colladerLeft.is_colliding()):
+			list.append(Vector2(-1,0))
+		if (!colladerDown.is_colliding()):
+			list.append(Vector2(0,1))
+		if (!colladerUp.is_colliding()):
+			list.append(Vector2(0,-1))
+		if list.size()>1:
+			if(list.has(oldDir)):
+				list.remove(list.find(oldDir))
+		if list.size()>0:	
+			dirXY = list[randi()%list.size()]
+			oldDir=dirXY*-1
 		canMove=false
 		var oldpos= position
-		var newpos=Vector2(oldpos.x  + (vec.x*64),oldpos.y + (vec.y*64))
+		var newpos=Vector2(oldpos.x  + (dirXY.x*64),oldpos.y + (dirXY.y*64))
 		tw.interpolate_property(self,"position",oldpos,newpos,0.6,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 		tw.start()
 		yield(tw,"tween_completed")
 		canMove =  true 
-		dir=monRandom(4)
-func monRandom(number:int)->int:
-	return randi()%number
-	pass
-	
-	
-
 
 func _on_hitbox_body_entered(body):
 	queue_free()
